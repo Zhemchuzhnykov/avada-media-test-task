a3954625b42c2098d7815a756f48bcae716de15b
Yevhen <zhemchuzhnykov@ukr.net>
2023-12-22 18:50:10 +0200

    Added the menu navigation for all the screen sizes, except for the movile screen size.

---
+++ src/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * Changes the background of a menu item
+ * and changes the text color
+ * when a mouse hovers over the item's link
+ *
+ * @param {Object} event - The event object
+ * @returns {void}
+ */
+function highlightMenuItem(event) {
+  let menuItem = event.target;
+
+  if (!menuItem.classList.contains("header-menu__menu-list-item")) {
+    menuItem = event.target.parentElement;
+  }
+
+  const menuLink = menuItem.firstElementChild;
+
+  menuItem.classList.toggle("highlighted");
+  menuLink.classList.toggle("highlighted");
+}
+
+const menuElement = document.getElementsByClassName(
+  "header-menu__menu-list"
+)[0];
+
+menuElement.addEventListener("mouseover", highlightMenuItem);
+menuElement.addEventListener("mouseout", highlightMenuItem);
